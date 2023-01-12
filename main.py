b23388faa0718d9cb5e585b05bf85256c0ffff54
Sch1561-it-5 <Sch1561-it-5@1561.ru>
2023-01-12 03:13:05 -1100

    Добавили вычитание

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def sum(a, b):
     return a + b
 
+def difference(a, b):
+    return a - b
 
 a, sign, b = input().split()
 
 if sign == "+":
     print(sum(int(a), int(b)))
+if sign == "-":
+    print(difference(int(a), int(b)))
