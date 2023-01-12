2e98e4556221f683f38de4558124752f9178a23b
sch1561-it-1 <sch1561-it-1@1561.ru>
2023-01-12 17:34:24 +0300

    add product

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ def sum(a, b):
 def difference(a, b):
     return a - b
 
+def product(a, b):
+    return a * b
+
+
 a, sign, b = input().split()
 
 if sign == "+":
     print(sum(int(a), int(b)))
 if sign == "-":
     print(difference(int(a), int(b)))
+if sign == "*":
+    print(product(int(a), int(b)))
